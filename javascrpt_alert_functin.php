65c571d93c61461752f22a8cae6e6c68c5c1974b
Xnu khan <97631565+Xnu9@users.noreply.github.com>
2023-11-16 16:43:26 +0500

    make it ok

---
+++ javascrpt_alert_functin.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<body>
+<h1>JavaScript Functions</h1>
+<script>
+alert("helo");
+alert(myFunction(1,2));
+function myFunction(p1, p2) {
+  return p1 * p2;
+}
+</script>
+
+</body>
+</html>
